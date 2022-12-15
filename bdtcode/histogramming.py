774b09e32388086f4b11092d680d18b6e40ec5eb
snabili <snabili@umd.edu>
2022-12-15 12:17:46 -0500

    test1

--- bdtcode/histogramming.py
+++ bdtcode/histogramming.py
@@ -46,13 +46,13 @@ def get_scores(rootfile, model, dataset_name=''):
                 ak8_pt   = event[b'JetsAK8.fCoordinates.fPt'][0]
                 muons = event[b'Muons']
                 electrons = event[b'Electrons']
-                print('get zprime')
-                zprime = get_zprime(event)
+                #print('get zprime')
+                #zprime = get_zprime(event)
                 #z_mass = zprime.mass
-                z_pt = zprime.pt
-                z_phi = zprime.phi
-                z_eta = zprime.eta
-                print('got zprime')
+                #z_pt = zprime.pt
+                #z_phi = zprime.phi
+                #z_eta = zprime.eta
+                #print('got zprime')
                 # adding ak4 jets leading and subleading jets
                 ak4_lead = get_ak4_lead(event)
                 ak4_subl = get_ak4_subl(event)
@@ -75,7 +75,8 @@ def get_scores(rootfile, model, dataset_name=''):
                     metphi, lead_pt, lead_phi, lead_eta,
                     subl.ecfM2b1, subl.ecfD2b1, subl.ecfC2b1, subl.ecfN2b2,
                     ak4_lead.eta, ak4_lead.phi, ak4_lead.pt, ak4_subl.eta, ak4_subl.phi, ak4_subl.pt,
-                    muons, electrons, ak8_pt, z_pt, z_phi, z_eta])
+                    muons, electrons, ak8_pt])
+                    #muons, electrons, ak8_pt, z_pt, z_phi, z_eta])
 
         except IndexError:
             print(f'Problem with {rootfile}; saving {cutflow["preselection"]} good entries')
@@ -91,8 +92,8 @@ def get_scores(rootfile, model, dataset_name=''):
 
         d = {k : np.array([]) for k in ['score', 'mt', 'rt', 'pt', 'met', 'phi', 'eta', 'metphi', 'lj_pt', 'lj_phi', 'lj_eta', 
                'subl.ecfM2b1', 'subl.ecfD2b1', 'subl.ecfC2b1', 'subl.ecfN2b2',
-               'ak4_lead.eta', 'ak4_lead.phi', 'ak4_lead.pt', 'ak4_subl.eta', 'ak4_subl.phi', 'ak4_subl.pt', 'muons', 'electrons', 'ak8_pt', 
-               'z_pt', 'z_phi', 'z_eta']}
+               'ak4_lead.eta', 'ak4_lead.phi', 'ak4_lead.pt', 'ak4_subl.eta', 'ak4_subl.phi', 'ak4_subl.pt', 'muons', 'electrons', 'ak8_pt']}
+               #'z_pt', 'z_phi', 'z_eta']}
 
         d.update(**cutflow.counts)
         d['wtime'] = t
@@ -119,7 +120,8 @@ def get_scores(rootfile, model, dataset_name=''):
                                                                   'lj_pt',  'lj_phi', 'lj_eta', 
                                                                   'subl.ecfM2b1', 'subl.ecfD2b1', 'subl.ecfC2b1', 'subl.ecfN2b2', 
                                                                   'ak4_lead.eta', 'ak4_lead.phi', 'ak4_lead.pt', 'ak4_subl.eta', 'ak4_subl.phi', 'ak4_subl.pt',
-                                                                  'muons', 'electrons', 'ak8_pt', 'z_pt', 'z_phi', 'z_eta'])},
+                                                                  'muons', 'electrons', 'ak8_pt'])},
+                                                                  #'muons', 'electrons', 'ak8_pt', 'z_pt', 'z_phi', 'z_eta'])},
 
         **cutflow.counts
         )
